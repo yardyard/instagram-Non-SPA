d01ceabb7d040c4dd96320a8ef2ff08c5ffbb1c8
yardyard <84224418+yardyard@users.noreply.github.com>
2022-01-04 16:06:43 +0900

    20220104

--- dongbaek/inflearn/models.py
+++ dongbaek/inflearn/models.py
@@ -1,9 +1,13 @@
 from typing import ContextManager
+from django.conf import settings
 from django.db import models
+from django.conf import settings
 
+from django.db.models.deletion import CASCADE
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="inflearn_author_set")
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
