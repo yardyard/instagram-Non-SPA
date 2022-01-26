4d4f58fd2f25ba77ce2e6b7f82e4be4697b42c6e
yardyard <84224418+yardyard@users.noreply.github.com>
2022-01-26 13:42:21 +0900

    Django 가상환경 기본 세팅

---
+++ django_instagram/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yardy\desktop\djangopractice\django_instagram\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
