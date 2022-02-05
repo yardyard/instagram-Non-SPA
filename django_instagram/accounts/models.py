0b6dca074732353419147fceaac0825ed46e8351
yardyard <84224418+yardyard@users.noreply.github.com>
2022-02-05 13:35:34 +0900

    send_grid 통한 이메일 발송

--- django_instagram/accounts/models.py
+++ django_instagram/accounts/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractUser
+from django.template.loader import render_to_string
 # Create your models here.
 
 class User(AbstractUser):
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
     
-
+    def send_welcome_email(self):        
+        Subject = render_to_string("accounts/welcome_email_subjects.txt", {
+            "user": self,
+        })
+        content = render_to_string("accounts/welcome_email_contents.txt", {
+            "user": self,
+        })
+        sender_email = settings.WELCOME_EMAIL_SENDER
+        send_mail( Subject, content , sender_email , [self.email], fail_silently=False)
 
 
 #class Profile(models.Model):
